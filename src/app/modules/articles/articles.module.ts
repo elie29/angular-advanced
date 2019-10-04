3b887cb73ce7840142d8fa3848c569abc934dd96
Elie NEHME <elie29@gmail.com>
2019-10-04 18:13:19 +0200

    Added articles containers

--- src/app/modules/articles/articles.module.ts
+++ src/app/modules/articles/articles.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { ArticleFormComponent } from './components/article-form/article-form.component';
 import { ArticleItemVotesComponent } from './components/article-item-votes/article-item-votes.component';
 import { ArticleItemComponent } from './components/article-item/article-item.component';
-import { ArticlesComponent } from './components/articles/articles.component';
+import { ArticlesComponent } from './containers/articles/articles.component';
 import { ArticlesService } from './services/articles.service';
 
 @NgModule({
