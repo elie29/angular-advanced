6aae7bcb5d1078da27b94ee0ef69ecc01cef90fb
Elie NEHME <elie29@gmail.com>
2019-10-05 14:23:42 +0200

    Added auth module

---
+++ src/app/modules/auth/modules/login/login.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { LoginFormComponent } from './containers/login-form/login-form.component';
+
+@NgModule({
+  imports: [],
+  declarations: [LoginFormComponent]
+})
+export class LoginModule {}
---
+++ src/app/modules/auth/modules/register/register.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { RegisterFormComponent } from './containers/register-form/register-form.component';
+
+@NgModule({
+  imports: [],
+  declarations: [RegisterFormComponent]
+})
+export class RegisterModule {}
