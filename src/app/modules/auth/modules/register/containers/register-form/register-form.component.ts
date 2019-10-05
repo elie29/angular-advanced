3eda6ad9f299c9bbb12e31e3b61c28d915e80811
Elie NEHME <elie29@gmail.com>
2019-10-05 16:20:03 +0200

    Added register form

--- src/app/modules/auth/modules/register/containers/register-form/register-form.component.ts
+++ src/app/modules/auth/modules/register/containers/register-form/register-form.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html'
 })
 export class RegisterFormComponent {
+  form: FormGroup = this.fb.group({
+    name: this.fb.group({
+      firstName: [''],
+      lastName: ['']
+    }),
+    email: [''],
+    password: [''],
+    languages: this.fb.array(['FR', 'EN'])
+  });
+
   // Error message
-  message = '';
+  message: string;
+
+  constructor(private fb: FormBuilder) {}
+
+  onRegister(): void {
+    this.message = '';
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
 }
--- src/app/modules/auth/modules/register/register.module.ts
+++ src/app/modules/auth/modules/register/register.module.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterFormComponent } from './containers/register-form/register-form.component';
@@ -6,7 +8,7 @@ import { RegisterFormComponent } from './containers/register-form/register-form.
 const ROUTES: Routes = [{ path: '', component: RegisterFormComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(ROUTES)],
+  imports: [RouterModule.forChild(ROUTES), ReactiveFormsModule, CommonModule],
   declarations: [RegisterFormComponent]
 })
 export class RegisterModule {}
