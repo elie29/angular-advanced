d33b99f24f138fc18eaf6de3b163dedc8e0d7678
Elie NEHME <elie29@gmail.com>
2019-10-05 15:17:09 +0200

    Added login form

--- src/app/modules/auth/modules/login/containers/login-form/login-form.component.ts
+++ src/app/modules/auth/modules/login/containers/login-form/login-form.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html'
 })
 export class LoginFormComponent {
+  @ViewChild('f', { static: false }) form: FormGroup;
+
   message = '';
+
+  onLogin(): void {
+    console.log(this.form.value);
+    this.form.reset();
+    this.message = 'Invalid email or password';
+  }
 }
--- src/app/modules/auth/modules/login/login.module.ts
+++ src/app/modules/auth/modules/login/login.module.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginFormComponent } from './containers/login-form/login-form.component';
@@ -6,7 +8,7 @@ import { LoginFormComponent } from './containers/login-form/login-form.component
 const ROUTES: Routes = [{ path: '', component: LoginFormComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(ROUTES)],
+  imports: [RouterModule.forChild(ROUTES), CommonModule, FormsModule],
   declarations: [LoginFormComponent]
 })
 export class LoginModule {}
