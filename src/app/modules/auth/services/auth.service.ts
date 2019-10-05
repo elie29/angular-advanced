b57215d5f5cea895fb596a371b0bfe78e932a49c
Elie NEHME <elie29@gmail.com>
2019-10-05 15:57:11 +0200

    Store injected

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@angular/core';
+import { createUser, User } from '@services/user.model';
+import { Store } from '@store';
 
 // needed in app and auth
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(private store: Store) {}
+
   loginUser(email: string, password: string): boolean {
-    if (email === 'admin@admin.com' && password === 'admin') {
-      localStorage.setItem('user', email);
-      return true;
-    }
-    return false;
+    const user = this.login(email, password);
+    this.store.set('user', user);
+    return user !== null;
   }
 
   logoutUser(): boolean {
+    this.store.set('user', null);
     localStorage.removeItem('user');
     return true;
+  }
+
+  private login(email: string, password: string): User | null {
+    if (email === 'admin@admin.com' && password === 'admin') {
+      localStorage.setItem('user', email);
+      return createUser(email);
+    }
+    return null;
   }
 }
